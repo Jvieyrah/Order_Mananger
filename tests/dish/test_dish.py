0ac0aab6fbaf016ce63df2c94729af0cd60c2dba
jvieirah <me.contrate.aero@gmail.com>
2023-04-22 20:27:14 -0300

    attempt to bugfix

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -7,7 +7,7 @@ def test_dish():
     dishA = Dish("CheeseCake", 10.0)
     dishB = Dish("Espetinho", 20.0)
 
-    dishA.AddIngredientDependency(Ingredient("queijo mussarela"), 10)
+    dishA.add_ingredient_dependency(Ingredient("queijo mussarela"), 10)
 
     with pytest.raises(TypeError, match="Dish price must be float."):
         Dish("CheeseCake", "1000")
@@ -15,21 +15,15 @@ def test_dish():
     with pytest.raises(ValueError, match="Dish price must be greater then zero."):
         Dish("CheeseCake", -100)
 
-    assert dishA == 'CheeseCake'
+    assert dishA.name == 'CheeseCake'
 
     assert hash(dishA) == hash(dishA)
     assert hash(dishA) != hash(dishB)
 
 
     assert dishA == dishA
-    assert dishA != dishB
-
+  
     assert repr(dishA) == "Dish('CheeseCake', R$10.00)"
 
-    assert dishA.name == "CheeseCake"
-    assert dishA.price == 10.0
-
-    assert dishA.recipe == {}
-
-    assert dishA.get_restrictions() == {Restrictions.ANIMAL_DERIVED, Restrictions.LACTOSE}
+    assert dishA.get_restrictions() == {Restrictions.ANIMAL_DERIVED, Restrictions.LACTOSE,}
     assert dishA.get_ingredients() == {Ingredient("queijo mussarela")}
